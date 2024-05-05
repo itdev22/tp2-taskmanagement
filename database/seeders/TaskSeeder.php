dd827acb6c1d0b8cdac995589a76826d48c45fc0
Rama <90917112+itdev22@users.noreply.github.com>
2024-05-06 01:11:22 +0700

    task seeder

--- database/seeders/TaskSeeder.php
+++ database/seeders/TaskSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Task;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,20 @@ class TaskSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Task::create([
+            'name' => 'Task 1',
+            'description' => 'Description for Task 1',
+            'user_id'=>2
+        ]);
+        Task::create([
+            'name' => 'Task 2',
+            'description' => 'Description for Task 2',
+            'user_id'=>2
+        ]);
+        Task::create([
+            'name' => 'Task 3',
+            'description' => 'Description for Task 3',
+            'user_id'=>2
+        ]);
     }
 }
